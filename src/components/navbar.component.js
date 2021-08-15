8cbf1e157030eb55b0161f4f0eb68a017c4f71f1
Liew Kuan Yung <liewkuanyung@gmail.com>
2021-08-15 23:58:40 +0800

    added components

---
+++ src/components/navbar.component.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Navbar extends Component {
+
+  render() {
+    return (
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <div className="container-fluid">
+          <Link to="/" className="navbar-brand">ExcerTracker</Link>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div className="collpase navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">Exercises</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/create" className="nav-link">Create Exercise Log</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/user" className="nav-link">Create User</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
